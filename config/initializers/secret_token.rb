219026be682f8397ac6358257194564680bee2f9
Daniel Kim <danielckim05@gmail.com>
2013-08-15 12:55:36 -0400

    Initial Rails Setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stocktrack::Application.config.secret_key_base = 'e32b70c6b660b487d9a19ab3a1981a28c3b0e99d4ca3fc9d2d895625c1ca48399ce2ace87f12dace42906cdd17c13af125d43b0f2d59f1c8c3e6c0f06c865bba'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Stocktrack::Application.config.session_store :cookie_store, key: '_stocktrack_session'
